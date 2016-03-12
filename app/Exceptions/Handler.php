3e9c44c4b574dd3054ffc5eeba1ff645b4862a9b
luismartinez24 <luismartinez242011@gmail.com>
2016-03-12 12:25:20 -0600

    feat: Errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,6 +38,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+         if ($e->getStatusCode() == 403) {
+            return response()->view('errors.403', [], 403);
+        }
+ 
         return parent::render($request, $e);
     }
 }
--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Error 403!!!</div>
             </div>
         </div>
     </body>
